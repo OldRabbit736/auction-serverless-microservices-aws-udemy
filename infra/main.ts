fc86813fa038fa2add54db41f96b6f5d02218f09
jspark <sylvan0212@gmail.com>
2021-08-26 22:27:36 +0900

    wow~

--- infra/main.ts
+++ infra/main.ts
@@ -2,6 +2,10 @@
 import "source-map-support/register";
 import * as cdk from "@aws-cdk/core";
 import { AuctionStack } from "./stacks/auction-stack";
+import { getConfig } from "./helper/get-config";
+
+// const filepath = getConfig();
+// console.log(filepath);
 
 const app = new cdk.App();
 new AuctionStack(app, "AuctionStack", {});
--- infra/stacks/auction-stack.ts
+++ infra/stacks/auction-stack.ts
@@ -1,9 +1,16 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
 
 export class AuctionStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new lambda.Function(this, "random-lambda", {
+      code: lambda.Code.fromAsset("codes/lambda"),
+      handler: "index.handler",
+      runtime: lambda.Runtime.NODEJS_14_X,
+      memorySize: 128,
+      timeout: cdk.Duration.minutes(2),
+    });
   }
 }
