fe425a380625a132dbd92c45e87463a38f0452ad
jspark <sylvan0212@gmail.com>
2021-08-26 21:39:37 +0900

    going on...

--- test/auction.test.ts
+++ test/auction.test.ts
@@ -1,13 +1,22 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as Auction from '../lib/auction-stack';
+import {
+  expect as expectCDK,
+  matchTemplate,
+  MatchStyle,
+} from "@aws-cdk/assert";
+import * as cdk from "@aws-cdk/core";
+import * as Auction from "../infra/stacks/auction-stack";
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new Auction.AuctionStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+test("Empty Stack", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new Auction.AuctionStack(app, "MyTestStack");
+  // THEN
+  expectCDK(stack).to(
+    matchTemplate(
+      {
+        Resources: {},
+      },
+      MatchStyle.EXACT
+    )
+  );
 });
