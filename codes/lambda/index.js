1c53e8e259a3cbc7296971298a5e7bc8b6044975
jspark <sylvan0212@gmail.com>
2021-08-26 23:14:28 +0900

    added API Gateway

--- codes/lambda/index.js
+++ codes/lambda/index.js
@@ -1,4 +1,6 @@
 exports.handler = async (event) => {
-  console.log(event);
-  console.log("hello");
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Hello!!!" }),
+  };
 };
--- infra/stacks/auction-stack.ts
+++ infra/stacks/auction-stack.ts
@@ -1,16 +1,29 @@
 import * as cdk from "@aws-cdk/core";
 import * as lambda from "@aws-cdk/aws-lambda";
+import * as apigw from "@aws-cdk/aws-apigateway";
 
 export class AuctionStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new lambda.Function(this, "random-lambda", {
+    const mylambda = new lambda.Function(this, "random-lambda", {
       code: lambda.Code.fromAsset("codes/lambda"),
       handler: "index.handler",
       runtime: lambda.Runtime.NODEJS_14_X,
       memorySize: 128,
       timeout: cdk.Duration.minutes(2),
+    });
+
+    const restApi = new apigw.RestApi(this, "Api", {
+      endpointTypes: [apigw.EndpointType.REGIONAL],
+    });
+
+    const hello = restApi.root.addResource("hello");
+    const helloIntegration = new apigw.LambdaIntegration(mylambda);
+    hello.addMethod("GET", helloIntegration);
+
+    new cdk.CfnOutput(this, "api_endpoint", {
+      value: restApi.url,
     });
   }
 }
