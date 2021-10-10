05165b0ae1856b61d5d662ba32d494a902321246
jspark <sylvan0212@gmail.com>
2021-10-10 10:38:34 +0900

    finished lesson 49

---
+++ script/token.sh
@@ -0,0 +1,7 @@
+curl --location --request POST 'https://dev-or.us.auth0.com/oauth/token' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--data-urlencode 'client_id=NcpsytQUgK1KdL7dZAId6j6DkrdWUEnN' \
+--data-urlencode 'username=sylvan0212@gmail.com' \
+--data-urlencode 'password=Password123!@#' \
+--data-urlencode 'grant_type=password' \
+--data-urlencode 'scope=openid'
